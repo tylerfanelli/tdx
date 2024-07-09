3cc7c9e08959165a3a4da17b5e6c016226b14d5d
Tyler Fanelli <tfanelli@redhat.com>
2024-07-09 11:53:11 -0400

    tests: Remove everything after capabilities fetch
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- tests/launch.rs
+++ tests/launch.rs
@@ -12,13 +12,4 @@ fn launch() {
     // create vm
     let tdx_vm = TdxVm::new(&kvm_fd, 100).unwrap();
     let caps = tdx_vm.get_capabilities().unwrap();
-    let _ = tdx_vm.init_vm(&kvm_fd, &caps).unwrap();
-
-    // create vcpu
-    let mut vcpufd = tdx_vm.fd.create_vcpu(10).unwrap();
-    let tdx_vcpu = TdxVcpu::try_from((&mut vcpufd, &mut kvm_fd)).unwrap();
-    let mut firmware = std::fs::File::open("./tests/data/OVMF.inteltdx.fd").unwrap();
-    let sections = tdvf::parse_sections(&mut firmware).unwrap();
-    let hob_section = tdvf::get_hob_section(&sections).unwrap();
-    tdx_vcpu.init(hob_section.memory_address).unwrap();
 }
